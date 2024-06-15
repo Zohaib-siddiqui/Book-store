9bb0436ce76320b7af3e962777e2b4ffde4570e1
Zohaib-siddiqui <zohaibsiddiqui60@gmail.com>
2024-06-16 03:31:47 +0500

    add scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,7 +118,12 @@ let swiperTestimonial = new Swiper('.testimonial__swiper', {
   });
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    window.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
 
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
