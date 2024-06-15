2d30f08c0ec822fb6b9250d8b7a07dff7da25a36
Zohaib-siddiqui <zohaibsiddiqui60@gmail.com>
2024-06-15 17:08:41 +0500

    add FEATURED

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,25 @@ let swiperHome = new Swiper('.home__swiper', {
   });
 
 /*=============== FEATURED SWIPER ===============*/
+let swiperfeatured = new Swiper('.featured__swiper', {
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+    breakpoints: {
+        1150: {
+            slidesPerView: 4,
+            centeredSlides:false,
+        }
+    }
+  });
 
 /*=============== NEW SWIPER ===============*/
 
