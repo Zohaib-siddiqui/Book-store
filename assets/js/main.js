89db556dd6649f7b85fbae6a061e3783c7ee9f1a
Zohaib-siddiqui <zohaibsiddiqui60@gmail.com>
2024-06-15 11:06:57 +0500

    Header and Search option

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
 /*=============== SEARCH ===============*/
+const searchButton = document.getElementById('search-button'),
+      searchClose = document.getElementById('search-close'),
+      searchContent = document.getElementById('search-content')
 
+    //   MENU SHOW
+    //   Validate if constant exists
+if (searchContent){
+    searchButton.addEventListener('click', () => {
+        searchContent.classList.add('show-search')
+    })
+}
 
+    //   MENU HIDDEN
+    //   Validate if constant exists
+if (searchClose){
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search')
+    })
+}
 /*=============== LOGIN ===============*/
 
 
