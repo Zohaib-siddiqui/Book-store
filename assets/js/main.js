30826822973a4387ba307e022a288c220ee8052d
Zohaib-siddiqui <zohaibsiddiqui60@gmail.com>
2024-06-15 11:57:13 +0500

    add login

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const searchButton = document.getElementById('search-button'),
       searchClose = document.getElementById('search-close'),
       searchContent = document.getElementById('search-content')
 
-    //   MENU SHOW
+    //   SEARCH SHOW
     //   Validate if constant exists
 if (searchContent){
     searchButton.addEventListener('click', () => {
@@ -11,7 +11,7 @@ if (searchContent){
     })
 }
 
-    //   MENU HIDDEN
+    //   SEARCH HIDDEN
     //   Validate if constant exists
 if (searchClose){
     searchClose.addEventListener('click', () => {
@@ -19,8 +19,25 @@ if (searchClose){
     })
 }
 /*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close'),
+      loginContent = document.getElementById('login-content')
 
+    //   LOGIN SHOW
+    //   Validate if constant exists
+if (loginContent){
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login')
+    })
+}
 
+    //   LOGIN HIDDEN
+    //   Validate if constant exists
+if (loginClose){
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login')
+    })
+}
 /*=============== ADD SHADOW HEADER ===============*/
 
 
